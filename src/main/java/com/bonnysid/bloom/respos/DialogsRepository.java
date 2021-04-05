254890b8c04192742019bc83796e0e1c7c5e872d
bonnysid <bonnysidworker@gmail.com>
2021-04-05 17:54:07 +0400

    feat: add Message and Dialogs repos

--- src/main/java/com/bonnysid/bloom/respos/DialogsRepository.java
+++ src/main/java/com/bonnysid/bloom/respos/DialogsRepository.java
@@ -2,6 +2,13 @@ package com.bonnysid.bloom.respos;
 
 import com.bonnysid.bloom.model.Dialog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface DialogsRepository extends JpaRepository<Dialog, Long> {
+
+    @Query(value = "select * from dialogs where id_from_user = ?1", nativeQuery = true)
+    public Optional<List<Dialog>> getAllByIdFromUser(Long id);
 }
--- src/main/java/com/bonnysid/bloom/respos/MessageRepository.java
+++ src/main/java/com/bonnysid/bloom/respos/MessageRepository.java
@@ -2,6 +2,12 @@ package com.bonnysid.bloom.respos;
 
 import com.bonnysid.bloom.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    @Query(value = "select * from message where id_dialog = ?1", nativeQuery = true)
+    public Optional<List<Message>> getMessagesByDialogId(Long id);
 }

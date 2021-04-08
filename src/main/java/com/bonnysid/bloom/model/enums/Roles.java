dee429eada486dae0bb09f7a748b2812b0408aeb
bonnysid <bonnysidworker@gmail.com>
2021-04-08 21:40:57 +0400

    ref: add permisions to some roles

--- src/main/java/com/bonnysid/bloom/model/enums/Roles.java
+++ src/main/java/com/bonnysid/bloom/model/enums/Roles.java
@@ -6,9 +6,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum Roles {
-    USER(Set.of(Permission.USER_READ)),
-    ADMIN(Set.of(Permission.USER_WRITE, Permission.USER_READ)),
-    MODERATOR(Set.of(Permission.USER_WRITE, Permission.USER_READ));
+    USER(Set.of(Permission.USER_READ, Permission.USER_WRITE)),
+    MODERATOR(Set.of(Permission.USER_WRITE, Permission.USER_READ, Permission.USER_UPDATE)),
+    ADMIN(Set.of(Permission.USER_WRITE, Permission.USER_READ, Permission.USER_UPDATE, Permission.USER_DELETE));
 
     public Set<Permission> getPermission() {
         return permission;
